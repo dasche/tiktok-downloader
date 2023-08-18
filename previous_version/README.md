70a8a9fa0f91444a01fa603fb103a7427ca6f355
Yosua Tarida Panjaitan <lomylix@gmail.com>
2023-08-18 20:57:30 +0700

    Adding the previous version dir

---
+++ previous_version/README.md
@@ -0,0 +1,42 @@
+
+# TikTok Downloader
+
+TikTok video and slideshow downloader, I create this project because I love watching TikTok videos. I can stay awake all night just because this one APP.
+
+I'm using Express, EJS, TailwindCSS, and [TikTok No Watermark API](https://github.com/siyosu/TikTok-No-Watermark-API) to get the content data.
+I'm new to Express and TailwindCSS, But I try both of them a few times before. Also, the more I dig deeper into TailwindCSS, the more I realized how little I know about CSS.
+
+Overall this is a fun project, now I can just go to my website and paste the video URL to download some TikTok videos.
+## Tech Stack
+
+**Client:** TailwindCSS
+
+**Server:** Node, Express, EJS
+
+
+## Installation
+
+Clone the project
+
+```bash
+  git clone https://github.com/siyosu/tiktok-downloader
+```
+
+Go to the project directory
+
+```bash
+  cd tiktok-downloader
+```
+
+Install dependencies
+
+```bash
+  npm install
+```
+
+Start the server
+
+```bash
+  npm run start
+```
+    
---
+++ previous_version/index.js
@@ -0,0 +1,56 @@
+const express = require('express')
+const flash = require('connect-flash')
+const session = require('express-session')
+require('dotenv/config')
+const TikTokNoWatermak = require('tiktok-no-watermark-api')
+const path = require('path')
+
+const app = express()
+
+app.set('view engine' ,'ejs')
+app.set('views', path.join(__dirname, 'views'))
+app.use('/', express.static(path.join(__dirname, 'public')))
+
+app.use(session({
+    secret: process.env.SECRET || 'fried chicken'
+}))
+app.use(flash())
+
+function abbreviateNumber(number) {
+    const SI_SYMBOLS = ["", "k", "m", "b"];
+    if (number < 1000) {
+      return number.toString();
+    }
+    const exp = Math.floor(Math.log10(number) / 3);
+    const abbreviated = (number / Math.pow(1000, exp)).toFixed(1);
+    return abbreviated + SI_SYMBOLS[exp];
+}
+
+app.get('/', (req, res)=>{
+    res.render('index', {message: req.flash('message')})
+})
+
+app.get('/download', (req, res)=>{
+    const {url} = req.query
+    if(!url || url.trim().length < 1 || url.length > 250){
+        req.flash('message', 'Video not found!')
+        res.redirect('/')
+    } else{
+        TikTokNoWatermak(url, true)
+        .then(data => {
+            data.result.details.total_views = abbreviateNumber(data.result.details.total_views)
+            data.result.details.total_likes = abbreviateNumber(data.result.details.total_likes)
+            data.result.details.total_comment = abbreviateNumber(data.result.details.total_comment)
+            res.render('download', {result: data.result})
+        })
+        .catch((err)=>{
+            console.log(err)
+            req.flash('message', 'Video not found!')
+            res.redirect('/')
+        })
+    }
+})
+
+const PORT = process.env.PORT || 5000
+
+app.listen(PORT, ()=> console.log(`Running on port ${PORT}`))
